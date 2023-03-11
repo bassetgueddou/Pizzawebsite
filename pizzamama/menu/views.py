0e31485252efe3441f18741315572bd3d67415db
bassetgueddou <bassetgueddou@gmail.com>
2023-03-11 14:09:21 +0100

    cs

--- pizzamama/menu/views.py
+++ pizzamama/menu/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Pizza
 
 # / menu
 
 
 def index(request):
-
-    return HttpResponse("Les pizzas")
+    pizzas = Pizza.objetcts.all()
+    pizzas_names = [pizza.nom for pizza in pizzas]
+    pizzas_names_str = ", ".join(pizzas_names)
+    return HttpResponse("Les pizzas : " + pizzas_names_str)
